fe27abec27e03f26c3af5da8c1064d5885c8934c
ilyakravec <iliakraff@gmail.com>
2023-09-04 15:08:44 +0800

    push

--- blocks/breadcrumbs-1/breadcrumbs.js
+++
@@ -1,9 +0,0 @@
-const breadcrumbItems = document.querySelectorAll('.breadcrumbs__inner__item');
-
-for (let i = 0; i < breadcrumbItems.length - 1; i++) {
-    const slash = document.createElement('div');
-    slash.classList.add('slash');
-    slash.textContent = '/';
-    slash.style.color = '#817b77';
-    breadcrumbItems[i].parentNode.insertBefore(slash, breadcrumbItems[i].nextSibling);
-}
--- blocks/patents-current-year-3/patents-current-year.js
+++ blocks/patents-current-year-3/patents-current-year.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         0:{
             items:1,
         },
-        725:{
+        777:{
             items:2,
             
         },
